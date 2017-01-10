67cfa7ae853d8fe9a95f71367dd72684f2e16f0a
Robert Farese <robertfarese@yahoo.com>
2017-01-10 15:51:35 -0500

    added ability for user to sign in with name attribute in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :role, inclusion: { in: %w(admin overnight_staff) }, presence: true
 
-  attr_accessor :login 
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "User sign in;", type: :feature do
+  let(:user) { FactoryGirl.create(:user) }
+
+  scenario "User views sign in form" do
+    visit new_user_session_path
+
+    expect(page).to have_field("Name")
+    expect(page).to have_field("Password")
+  end
+
+  scenario "User successfully signs in" do
+    visit new_user_session_path
+
+    fill_in "Name", with: user.name
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect(page).to have_content("Signed in successfully")
+  end
+
+  scenario "User does not provide valid information to authenticate" do
+    visit new_user_session_path
+
+    fill_in "Name", with: ""
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect(page).to have_content("Invalid")
+  end
+end
