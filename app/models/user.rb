c3227e8c90effe6c80dd4fb0c1bb083a75cf42e4
Robert Farese <robertfarese@yahoo.com>
2017-01-10 14:17:29 -0500

    added new session as root; updated devise auth to use name attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true
-  validates :role, inclusion: { in: %w(admin overnight_staff) }, presence: true 
+  validates :role, inclusion: { in: %w(admin overnight_staff) }, presence: true
+
+  attr_accessor :login 
 end
