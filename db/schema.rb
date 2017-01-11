428eff1b770b8d916607ae1426ddf6c2bbe3b2d5
Robert Farese <robertfarese@yahoo.com>
2017-01-11 12:15:27 -0500

    added student model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111165602) do
+ActiveRecord::Schema.define(version: 20170111170617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,15 @@ ActiveRecord::Schema.define(version: 20170111165602) do
   end
 
   add_index "rooms", ["house_id"], name: "index_rooms_on_house_id", using: :btree
+
+  create_table "students", force: :cascade do |t|
+    t.string   "initials",   limit: 3, null: false
+    t.integer  "bed_id"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "students", ["bed_id"], name: "index_students_on_bed_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",                null: false
