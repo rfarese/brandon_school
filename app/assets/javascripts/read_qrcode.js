d230569071933082bb638a5d3ad8efdf8a244178
Robert Farese <robertfarese@yahoo.com>
2017-01-12 12:01:55 -0500

    removed AJAX call from read_qrcode.js

--- app/assets/javascripts/read_qrcode.js
+++ app/assets/javascripts/read_qrcode.js
@@ -1,25 +1,6 @@
-function makeAjaxCall(room_identifier) {
-	var request = $.ajax( {
-	  method: "POST",
-	  url: "http://localhost:3000/api/v1/some_end_point",
-		data: {
-			tour: {
-				room_id: room_identifier
-			}
-		}
-	});
-
-	request.done(function(data) {
-		$("#decoded-room-id").append(data.room_id);
-		$("#decoded-room-name").append(data.room_name);
-		$("#decoded-house-name").append(data.house_name);
-	});
-}
-
 function onSuccess(data) {
 		document.getElementById('video').setAttribute("style", "border: 3px solid #52e250");
 		console.log('Sucess:', data);
-		// makeAjaxCall(data);
 }
 
 function onError(err) {
