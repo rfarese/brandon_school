78a79e91fca4091741a6024f9e0fa3a8d600f8d0
Robert Farese <robertfarese@yahoo.com>
2017-01-13 14:10:57 -0500

    need to fix this branch

--- spec/controllers/api/v1/student_checks_controller_spec.rb
+++
@@ -1,6 +0,0 @@
-RSpec.describe Api::V1::StudentChecksController, type: :controller do
-  describe "POST /api/v1/student_checks" do
-    it "creates a new group of student checks"
-    it "returns 'not_found' if validations fail"
-  end
-end
