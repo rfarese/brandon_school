aa02ea519b3bd9276b3df0df849678192a1212f9
Robert Farese <robertfarese@yahoo.com>
2017-01-20 18:51:16 -0500

    removed comments and unused js files

--- app/assets/javascripts/read_qrcode.js
+++ app/assets/javascripts/read_qrcode.js
@@ -6,55 +6,24 @@ function generateStudentCheckButton(room_id) {
 	button.show();
 }
 
-// function generateStudentChecks(data) {
-// 	var request = $.ajax( {
-// 	  method: "POST",
-// 	  url: "http://localhost:3000/generate_student_checks",
-// 		data: {
-// 			student_check: data
-// 		}
-// 	});
-//
-// 	request.done(function(data) {
-// 		// redirect to new student_checks page and populate the forms
-// 		var students = data.student_checks
-// 		studentQuantity = students.length
-// 		formsSubmitted = 0;
-// 		var form = $(".student-check-form");
-// 		form.show();
-//
-// 		for (var i = 0; i < students.length; i++) {
-// 			student = students[i]
-// 			if (i === 0 ) {
-// 				addStudentToForm(student)
-// 			} else {
-// 				createStudentForm(student)
-// 			};
-// 		};
-// 	});
-// }
-
 function onSuccess(data) {
-		document.getElementById('video').setAttribute("style", "border: 3px solid #52e250");
-		console.log('Sucess:', data);
-		// var tourId = $(".tour_id").text();
-		// var generatorData = { room_id: data, tour_id: tourId }
-		// generateStudentChecks(generatorData)
-		generateStudentCheckButton(data)
+	document.getElementById('video').setAttribute("style", "border: 3px solid #52e250");
+	console.log('Sucess:', data);
+	generateStudentCheckButton(data)
 }
 
 function onError(err) {
-		console.error(err);
+	console.error(err);
 }
 
 $(document).ready(function() {
 	QrReader.getBackCamera().then(function(device) {
 		new QrReader({
-				sucessCallback: onSuccess, // Required
-				errorCallback: onError, // Required
-				videoSelector: '#video', // If not provided creates an invisible element and decode in background
-				stopOnRead: true, // Default false, When true the video will stop once the first QR is read.
-				deviceId: device.deviceId, // Id of the device used for recording video.
+			sucessCallback: onSuccess, // Required
+			errorCallback: onError, // Required
+			videoSelector: '#video', // If not provided creates an invisible element and decode in background
+			stopOnRead: true, // Default false, When true the video will stop once the first QR is read.
+			deviceId: device.deviceId, // Id of the device used for recording video.
 		});
 	});
 });
