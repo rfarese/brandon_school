90ca303d2014602f6298125be06fa1239a4e1c7c
Robert Farese <robertfarese@yahoo.com>
2017-01-25 16:00:45 -0500

    added house index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resource :qrcode_scans, only: [:new]
   resources :rooms, only: [:new, :create]
   resources :student_checks, only: [:update]
+  resources :houses, only: [:index, :new, :create, :show]
 
   post 'generate_student_checks' => 'student_checks#new'
 end
