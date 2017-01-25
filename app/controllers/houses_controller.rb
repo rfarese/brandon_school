9174a619a84a51c7b9847201d4a8dc5b400bd72e
Robert Farese <robertfarese@yahoo.com>
2017-01-25 16:30:03 -0500

    added ability for user to create a new house

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -2,4 +2,24 @@ class HousesController < ApplicationController
   def index
     @houses = House.all
   end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.new(house_params)
+    if @house.save
+      flash[:notice] = "New House Saved"
+      redirect_to houses_path
+    else
+      flash[:notice] = "House Not Saved"
+      render 'new'
+    end
+  end
+
+  private
+  def house_params
+    params.require(:house).permit(:name)
+  end
 end
