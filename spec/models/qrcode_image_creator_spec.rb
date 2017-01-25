9267d09f3ed6221f1dbf915c448af8b4df394f3d
Robert Farese <robertfarese@yahoo.com>
2017-01-25 08:41:08 -0500

    refactored qrcode; added qrcode image creator poro

---
+++ spec/models/qrcode_image_creator_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+require_relative '../../app/models/qrcode_image_creator'
+
+RSpec.describe QrCodeImageCreator, type: :model do
+  let(:room) { FactoryGirl.create(:room) }
+
+  describe "#build_image" do
+    it "builds a QR code image" do
+      qrcode = QrCodeImageCreator.new(room.id.to_s)
+      qrcode.build_image
+
+      expect(qrcode.room_id).to eq(room.id.to_s)
+    end
+  end
+end
