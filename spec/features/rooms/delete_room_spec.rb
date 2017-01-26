b8dff6848d4934c6d7da96dcff0e520c76da495d
Robert Farese <robertfarese@yahoo.com>
2017-01-26 13:37:59 -0500

    added ability to delete a room

---
+++ spec/features/rooms/delete_room_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "User views all rooms;", type: :feature do
+  let(:permission) { FactoryGirl.create(:permission) }
+  let(:user) { permission.user }
+  let(:house) { permission.house }
+  let(:room)  { FactoryGirl.create(:room, house_id: house.id) }
+  let(:room2) { FactoryGirl.create(:room, house_id: house.id) }
+
+  def sign_in_and_navigate_to_rooms_index
+    user.role = "admin"
+    sign_in(user)
+    click_link "Rooms"
+  end
+
+  scenario "User views ability to delete a room" do
+    room
+    sign_in_and_navigate_to_rooms_index
+
+    expect(page).to have_content("delete")
+  end
+
+  # NOTE - need phantom.js for both of these tests below
+
+  scenario "User successfully deletes a room"
+
+  scenario "User recieves a notice before deleting a room"
+end
