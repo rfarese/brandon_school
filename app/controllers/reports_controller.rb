b98a99213bfce7e19ba24112489d0298b9e987fe
Robert Farese <robertfarese@yahoo.com>
2017-02-06 08:07:28 -0500

    added report poro

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,41 +5,44 @@ class ReportsController < ApplicationController
 
   def filters
     filters = params[:filters]
-    args = generate_args(filters)
-    status = params[:filters][:status]
+    # args = generate_args(filters)
+    # status = params[:filters][:status]
     @houses = House.all
-    @student_checks = student_check_generator(args, filters[:status])
+    # @student_checks = student_check_generator(args, filters[:status])
+
+    report_data = ReportGenerator.new(filters)
+    @student_checks = report_data.build_student_checks
 
     render 'index'
   end
 
-  private
-
-  def generate_args(filters)
-    { start_date: new_start_date(filters),
-      end_date: new_end_date(filters),
-      houses: find_houses(filters),
-      statuses: filters[:student_status] }
-  end
-
-  def find_houses(filters)
-    House.where(id: filters[:houses])
-  end
-
-  def new_start_date(filters)
-    Date.new(filters["start_date(1i)"].to_i,
-      filters["start_date(2i)"].to_i,
-      filters["start_date(3i)"].to_i)
-  end
-
-  def new_end_date(filters)
-    Date.new(filters["end_date(1i)"].to_i,
-      filters["end_date(2i)"].to_i,
-      filters["end_date(3i)"].to_i)
-  end
-
-  def student_check_generator(args, status)
-    return StudentCheck.send(status).filters(args) unless status == "all"
-    StudentCheck.filters(args)
-  end
+  # private
+  #
+  # def generate_args(filters)
+  #   { start_date: new_start_date(filters),
+  #     end_date: new_end_date(filters),
+  #     houses: find_houses(filters),
+  #     statuses: filters[:student_status] }
+  # end
+  #
+  # def find_houses(filters)
+  #   House.where(id: filters[:houses])
+  # end
+  #
+  # def new_start_date(filters)
+  #   Date.new(filters["start_date(1i)"].to_i,
+  #     filters["start_date(2i)"].to_i,
+  #     filters["start_date(3i)"].to_i)
+  # end
+  #
+  # def new_end_date(filters)
+  #   Date.new(filters["end_date(1i)"].to_i,
+  #     filters["end_date(2i)"].to_i,
+  #     filters["end_date(3i)"].to_i)
+  # end
+  #
+  # def student_check_generator(args, status)
+  #   return StudentCheck.send(status).filters(args) unless status == "all"
+  #   StudentCheck.filters(args)
+  # end
 end
