9bb0db3c162baff102c47f6e2e685e5167e5ec03
Robert Farese <robertfarese@yahoo.com>
2017-03-01 10:59:42 -0500

    removed require csv

--- app/assets/javascripts/csv_export.js
+++ app/assets/javascripts/csv_export.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   if (top.location.pathname === "/report_filters" ) {
     // collect the report results into a series of variables
-    var unsplitHeaders = $("#report-results-table").find("th").text();
-    var headers = unsplitHeaders.split(" "); 
+    // var unsplitHeaders = $("#report-results-table").find("th").text();
+    // var headers = unsplitHeaders.split(" ");
+    var table = $("#report-results-table")
+    var headers = table.find("tr:has(th)");
+    var rows = table.find("tr:has(td)"); 
+    debugger;
   }
 });
