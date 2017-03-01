c458cbeb00dff5b43e11d900fe80b55b3bc2f6d6
Robert Farese <robertfarese@yahoo.com>
2017-02-28 20:26:38 -0500

    updated tour timer

--- app/assets/javascripts/tour_timer.js
+++ app/assets/javascripts/tour_timer.js
@@ -1,27 +1,16 @@
 $(document).ready(function() {
   if (top.location.pathname === "/tours" || top.location.pathname == "/generate_student_checks" || top.location.pathname.includes("/student_checks/")) {
-    // set the time we are counting down too
     var tourStartTime = $(".tour-time").text();
 
     var initialDate = new Date(tourStartTime);
-    var tourEndTime = new Date(initialDate.getTime() + (1*60*1000));
+    var tourEndTime = new Date(initialDate.getTime() + (20*60*1000));
 
-    // Update the count down every second
     var x = setInterval(function(){
-      // Get todays date and time
       var now = new Date().getTime();
-
-      // find the distance between now and the tour end time
       var distance = tourEndTime - now;
-
-      // Time calculations for days, hours, minutes, and seconds
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      // Display the results in the element with id='demo'
-      document.getElementById("demo").innerHTML = minutes + " Minutes " + seconds + " Seconds ";
-
-      // If the count down is finished, redirect to root path and display message that the tour has expired
+      document.getElementById("countdown").innerHTML = minutes + " Minutes " + seconds + " Seconds ";
 
       if (distance < 0) {
         clearInterval(x);
