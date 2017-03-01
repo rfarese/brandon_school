59a53b7bfbfe29188cf9cd2278e1fc19e8df84d4
Robert Farese <robertfarese@yahoo.com>
2017-03-01 09:33:17 -0500

    added pagination to reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,6 +8,7 @@ class ReportsController < ApplicationController
     @houses = House.all
     report_data = ReportGenerator.new(filters)
     @student_checks = report_data.build_student_checks
+    @student_checks = Kaminari.paginate_array(@student_checks).page(params[:page]).per(10)
 
     render 'index'
   end
