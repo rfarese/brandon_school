59b582799667a8e2f4766cd08aade56e5dc119bd
Robert Farese <robertfarese@yahoo.com>
2017-03-02 20:41:46 -0500

    removed pry

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,5 @@
 class HomesController < ApplicationController
   def index
-    binding.pry
     if user_signed_in? && current_user.role == "overnight_staff"
       house = current_user.houses.first
       @tour = house.tours.last
