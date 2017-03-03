eebbce955fdb66aacc62849ad0be212c8e0f3449
Robert Farese <robertfarese@yahoo.com>
2017-03-03 18:36:20 -0500

    fixed time submital to convert est to utc

--- app/models/report_generator.rb
+++ app/models/report_generator.rb
@@ -7,14 +7,6 @@ class ReportGenerator
     @args = generate_args(filters)
   end
 
-  # you should just be able to remove @args = generate_args(filters) from initialize
-  # then, in generate_args method, just add @args = { start_date: ... }
-  # then you can remove all the (filters) arguments and parameters to
-  # generate_args, find_houses, new_start_date, and new_end_date
-  # however, you will have to look in your controllers and see where you'll have
-  # to call generate_args because you are calling it automatically when the object
-  # is instanciated 
-
   def generate_args(filters)
     { start_date: new_start_date(filters),
       end_date: new_end_date(filters),
@@ -27,15 +19,19 @@ class ReportGenerator
   end
 
   def new_start_date(filters)
-    Date.new(filters["start_date(1i)"].to_i,
-      filters["start_date(2i)"].to_i,
-      filters["start_date(3i)"].to_i)
+    start_date = Date.new(filters["start_date(1i)"].to_i,
+                          filters["start_date(2i)"].to_i,
+                          filters["start_date(3i)"].to_i)
+
+    (start_date.to_time + 5.hours).to_datetime
   end
 
   def new_end_date(filters)
-    Date.new(filters["end_date(1i)"].to_i,
-      filters["end_date(2i)"].to_i,
-      filters["end_date(3i)"].to_i)
+    end_date = Date.new(filters["end_date(1i)"].to_i,
+                        filters["end_date(2i)"].to_i,
+                        filters["end_date(3i)"].to_i)
+
+    (end_date.to_time + 5.hours).to_datetime
   end
 
   def build_student_checks
