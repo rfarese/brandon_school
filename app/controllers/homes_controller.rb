132aca6e9b460c1e37384dfa98d088425412b2e0
Robert Farese <robertfarese@yahoo.com>
2017-03-03 13:59:51 -0500

    removed tour instance from root path

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,4 @@
 class HomesController < ApplicationController
   def index
-    if user_signed_in? && current_user.role == "overnight_staff"
-      house = current_user.houses.first
-      @tour = house.tours.last
-    end
   end
 end
