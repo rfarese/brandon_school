f51d45678a5966dcf4e10761724ce5d0067ed99b
Robert Farese <robertfarese@yahoo.com>
2017-04-05 07:07:57 -0400

    removed comments

--- app/models/house.rb
+++ app/models/house.rb
@@ -5,12 +5,6 @@ class House < ActiveRecord::Base
   has_many :tours
   validates :name, presence: true
 
-  # instead of iterating over rooms and then over beds, try this...
-  # add a class method onto the Bed model
-  # have that class method return all the beds for a given house
-  # inside the beds method below (in the house model), pass the house (self)
-  # as a parameter to the bed class method
-
   scope :tours_today, -> { includes(:tours).joins(:tours).merge(Tour.today) }
 
   def beds
