23831e146fe8a9fffbe2718c70019f784b3601ab
Robert Farese <robertfarese@yahoo.com>
2017-04-05 16:37:35 -0400

    added cache for report filters to render xls

--- app/models/student_check.rb
+++ app/models/student_check.rb
@@ -27,4 +27,13 @@ class StudentCheck < ActiveRecord::Base
   def house
     tour.house
   end
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |student_check|
+        csv << student_check.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
