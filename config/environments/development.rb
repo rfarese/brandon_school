a484d7e5f83d71873f813d25498a5075aad2ec7f
Robert Farese <robertfarese@yahoo.com>
2017-04-07 09:10:47 -0400

    working on fixing issues outlined by bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.cache_classes = false
+  config.cache_classes = false  
   config.eager_load = false
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
@@ -21,4 +21,12 @@ Rails.application.configure do
   config.assets.debug = true
   config.assets.digest = true
   config.assets.raise_runtime_errors = true
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
 end
