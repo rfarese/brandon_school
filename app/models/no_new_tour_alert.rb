c7b975f018426ea956278471f80f2feb80b96d78
Robert Farese <robertfarese@yahoo.com>
2017-04-08 14:26:19 -0400

    changed no_new_tour_alert and email notification rake task

--- app/models/no_new_tour_alert.rb
+++ app/models/no_new_tour_alert.rb
@@ -1,34 +1,39 @@
 class NoNewTourAlert
   attr_reader :current_time, :house_name, :tour
+  # attr_accessor :current_time
 
   def initialize(args={})
     @house_name = args[:house_name]
     @tour = args[:tour]
-    @current_time = set_current_time
+    @current_time = Time.now
   end
 
-  def is_edt?
-    Time.now + 4.hour if Time.now.zone == "EDT"
-  end
+  # def is_edt?
+  #   Time.now + 4.hour if Time.now.zone == "EDT"
+  # end
+  #
+  # def is_est?
+  #   Time.now + 5.hour if Time.now.zone == "EST"
+  # end
+  #
+  # def set_current_time
+  #   is_edt? || is_est?
+  # end
 
-  def is_est?
-    Time.now + 5.hour if Time.now.zone == "EST"
-  end
+  # def correct_time?
+  #   between_11_and_12? || between_12_and_6?
+  # end
+  #
+  # def between_11_and_12?
+  #   current_time >= (Time.parse "11:00 pm") && current_time <= (Time.parse "11:59:59 pm")
+  # end
 
-  def set_current_time
-    is_edt? || is_est?
+  def should_send_alert?
+    correct_time? && time_passed_too_long? 
   end
 
   def correct_time?
-    between_11_and_12? || between_12_and_6?
-  end
-
-  def between_11_and_12?
-    current_time >= (Time.parse "11:00 pm") && current_time <= (Time.parse "11:59:59 pm")
-  end
-
-  def between_12_and_6?
-    current_time >= (Time.parse "12:00 am") && current_time <= (Time.parse "6:05 am")
+    current_time >= (Time.parse "12:25 am") && current_time <= (Time.parse "6:35 am")
   end
 
   def time_passed_too_long?
