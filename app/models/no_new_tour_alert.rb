2c9bf11322df9f35d9a3b3d34e43d95e660defe2
Robert Farese <robertfarese@yahoo.com>
2017-04-08 14:58:58 -0400

    removed commented out code in NoNewTourAlert

--- app/models/no_new_tour_alert.rb
+++ app/models/no_new_tour_alert.rb
@@ -1,6 +1,5 @@
 class NoNewTourAlert
   attr_reader :current_time, :house_name, :tour
-  # attr_accessor :current_time
 
   def initialize(args={})
     @house_name = args[:house_name]
@@ -8,28 +7,8 @@ class NoNewTourAlert
     @current_time = Time.now
   end
 
-  # def is_edt?
-  #   Time.now + 4.hour if Time.now.zone == "EDT"
-  # end
-  #
-  # def is_est?
-  #   Time.now + 5.hour if Time.now.zone == "EST"
-  # end
-  #
-  # def set_current_time
-  #   is_edt? || is_est?
-  # end
-
-  # def correct_time?
-  #   between_11_and_12? || between_12_and_6?
-  # end
-  #
-  # def between_11_and_12?
-  #   current_time >= (Time.parse "11:00 pm") && current_time <= (Time.parse "11:59:59 pm")
-  # end
-
   def should_send_alert?
-    correct_time? && time_passed_too_long? 
+    correct_time? && time_passed_too_long?
   end
 
   def correct_time?
