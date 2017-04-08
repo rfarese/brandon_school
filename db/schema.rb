55d9b430cc45867d8e169623ffe8f4f670f16d1a
Robert Farese <robertfarese@yahoo.com>
2017-04-08 10:04:18 -0400

    added beds counter cache to house

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408134621) do
+ActiveRecord::Schema.define(version: 20170408135846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170408134621) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.integer  "rooms_count", default: 0, null: false
+    t.integer  "beds_count",  default: 0, null: false
   end
 
   create_table "permissions", force: :cascade do |t|
