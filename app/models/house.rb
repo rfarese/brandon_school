6c4c57008e6c99c100c0678ab07a1f2c4d4d0a0d
Robert Farese <robertfarese@yahoo.com>
2017-04-25 10:30:53 -0400

    removed last_night_tours from house

--- app/models/house.rb
+++ app/models/house.rb
@@ -10,10 +10,6 @@ class House < ActiveRecord::Base
   # and we should just includes(:house) for the scope on the Tour model...
   scope :tours_today, -> { includes(:tours).joins(:tours).merge(Tour.today) }
 
-  # I don't even think we are using this scope...I think we are just using
-  # last_night scope on the Tour model 
-  scope :last_night_tours, -> { includes(:tours).joins(:tours).merge(Tour.last_night) }
-
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
       csv << column_names
